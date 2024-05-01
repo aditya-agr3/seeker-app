89c4d1aaae14a3b8b05d8d3c25ffaf5764ec9d43
Viral-Gajera <viral.gajera218@gmail.com>
2024-05-01 12:59:59 +0530

    back button functionality added

--- source/app/shared/components/navbar/navbar.component.ts
+++ source/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-navbar',
@@ -13,10 +14,16 @@ export class NavbarComponent {
     @Input() hero!: boolean;
     isScrolled: boolean = false;
 
+    constructor(private router: Router) {}
+
     @HostListener('window:scroll', [])
     onWindowScroll() {
         // If the navbar is in hero mode, don't change the background color
         this.isScrolled = window.pageYOffset >= window.innerHeight;
         // Detect if the user has scrolled down 100vh
+    }
+
+    goBack() {
+        this.router.navigate(['../']); // Go back one level in the route hierarchy
     }
 }
