616907936c0c498e9c43b08f25f38fdba04c3406
Viral-Gajera <viral.gajera218@gmail.com>
2024-05-01 18:22:21 +0530

    details page modified

--- source/app/app.routes.ts
+++ source/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { AboutUsComponent } from './views/about-us/about-us.component';
+import { ContentDetailComponent } from './views/content-detail/content-detail.components';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about-us', component: AboutUsComponent },
+    { path: 'details', component: ContentDetailComponent },
 ];
