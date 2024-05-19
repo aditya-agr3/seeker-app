6f9d5ffe86e747bbbf8cb0d7a47a40d0084e3e59
Viral-Gajera <viral.gajera218@gmail.com>
2024-05-19 23:04:57 +0530

    select api updated

--- source/app/core/services/select.service.ts
+++ source/app/core/services/select.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environment/environment';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
     providedIn: 'root',
@@ -21,9 +22,9 @@ export class SelectService {
                 bap_uri: 'https://kahani-bap.tekdinext.com/',
                 bpp_id: 'kahani-bpp.tekdinext.com',
                 bpp_uri: 'https://kahani-bpp.tekdinext.com/',
-                transaction_id: 'b2e2c51d-d29a-4873-87f8-d7a81bbba283',
-                message_id: '781cb49a-64a9-487e-82f9-f939705e1f3d',
-                timestamp: '2024-05-14T11:39:17.721Z',
+                transaction_id: uuidv4(),
+                message_id: uuidv4(),
+                timestamp: new Date().toISOString(),
             },
             message: {
                 order: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./source/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./source/**/*.{html,ts}"],
+    theme: {
+        extend: {
+            colors: {
+                "c-1": "#73d1ff",
+                "c-1.1": "#5ca7cc",
+            },
+        },
+    },
+    plugins: [],
 };
