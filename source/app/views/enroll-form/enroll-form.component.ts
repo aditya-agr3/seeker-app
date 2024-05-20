de623ac3b4e81f6a8bcaa1a0df802d22c0e1b67e
Viral-Gajera <viral.gajera218@gmail.com>
2024-05-20 23:19:23 +0530

    init & confirm API handler added

--- source/app/views/enroll-form/enroll-form.component.ts
+++ source/app/views/enroll-form/enroll-form.component.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { InitService } from '../../core/services/init.service';
 
 @Component({
     selector: 'app-enroll-form',
     standalone: true,
-    imports: [FooterComponent, NavbarComponent],
+    imports: [
+        FooterComponent,
+        NavbarComponent,
+        ReactiveFormsModule,
+        CommonModule,
+    ],
     templateUrl: './enroll-form.component.html',
     styleUrl: './enroll-form.component.css',
 })
-export class EnrollFormComponent {}
+export class EnrollFormComponent {
+    isLoading: boolean = false;
+    enrollForm!: FormGroup;
+    providerId!: string;
+    itemId!: string;
+
+    constructor(
+        private fb: FormBuilder,
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private initService: InitService
+    ) {
+        this.activatedRoute.queryParams.subscribe((params) => {
+            this.providerId = params['provider_id'];
+            this.itemId = params['item_id'];
+        });
+    }
+
+    ngOnInit() {
+        this.enrollForm = this.fb.group({
+            name: ['', Validators.required],
+            age: ['', [Validators.required, Validators.min(18)]],
+            email: ['', [Validators.required, Validators.email]],
+            phone: ['', Validators.required],
+        });
+    }
+    onSubmit() {
+        this.isLoading = true;
+        this.initService
+            .submitDetails(this.providerId, this.itemId, this.enrollForm.value)
+            .subscribe((data) => {
+                this.isLoading = false;
+                // navigate to /course
+                this.router.navigate(['/course'], {
+                    queryParams: {
+                        provider_id: this.providerId,
+                        item_id: this.itemId,
+                    },
+                });
+            });
+    }
+}
